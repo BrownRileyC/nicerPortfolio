4e83bbf7b698dbe042bc3bd8b74bc5a37371a070
Riley Brown <brown.riley.c@gmail.com>
2019-03-21 12:43:31 -0400

    misnamed server

--- routes/Router.js
+++ routes/Router.js
@@ -4,11 +4,3 @@ var router = express.Router();
 router.get('/', function (req, res) {
     res.render('index');
 });
-
-router.get('/portfolio', function(req, res) {
-    res.render('projects');
-});
-
-router.get('/contact', function (req, res) {
-    res.render('contact');
-});
