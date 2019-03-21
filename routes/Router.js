9a280e36636552765ccd364668e2b2606df3aa0a
Riley Brown <brown.riley.c@gmail.com>
2019-03-21 12:57:21 -0400

    updated the paths

--- routes/Router.js
+++ routes/Router.js
@@ -1,6 +1,9 @@
-var express = requier('express');
+var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('index');
+    res.sendFile(path.join(__dirname, '../public/assets/index.html'));
 });
+
+module.exports = router;
